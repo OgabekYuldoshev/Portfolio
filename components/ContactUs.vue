e18abd58e217fe79609b64b207c6128581efa441
OgabekYuldoshev <yuldashoff1gmail.com>
2021-09-06 05:05:50 -0700

    asd

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -7,10 +7,10 @@
       {{ $t("contacts") }}
     </h1>
     <div
-      data-aos="fade-right"
       class="container px-5 py-5 mx-auto flex sm:flex-nowrap flex-wrap"
     >
       <div
+        data-aos="fade-right"
         class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative"
       >
         <iframe
@@ -55,6 +55,7 @@
         </div>
       </div>
       <div
+      data-aos="fade-left"
         class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
       >
         <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
